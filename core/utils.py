3550719bae1aa1c86bcf80bb7c3c073cca7498e2
kayinsg <kayin.gayle@gmail.com>
2025-02-23 19:13:14 -0500

    chore(Core): remove needless util functions

--- core/utils.py
+++ core/utils.py
@@ -2,26 +2,6 @@ from pathlib import Path
 import pyperclip
 import re as regex
 
-
-def fetchDirectoriesInPath(path: Path):
-    elementsInDirectory = list(path.iterdir())
-
-    foldersInPath = list()
-
-    for pathInstance in elementsInDirectory:
-        fullPath = pathInstance.as_posix()
-        if pathInstance.is_dir():
-            foldersInPath.append(fullPath)
-
-    return foldersInPath
-
-def getListOfLinesFromFile(filePath):
-    if isinstance(filePath, Path):
-        return filePath.read_text().splitlines()
-    else:
-        return Path(filePath).read_text().splitlines()
-
-
 def copyPathToClipboard(filePath):
     sanitizedFilePath = filePath.strip()
     enclosedPath = f'"{sanitizedFilePath}"'
